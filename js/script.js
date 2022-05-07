5ba817bd76de75d28933e5ae32317f430a11c5c4
Thomas Zeng <tomzeng200@gmail.com>
2022-05-07 15:16:17 -0500

    implements html scaffolding and basic js
    implements the basic html form structure to add a new book
    implements rudimentary book constructor
    implements rudimentary js method to reset book form after submission

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 let myLibrary = [];
 
+function Book(author,title,pages,readState){
+    this.author=author;
+    this.title=title;
+    this.pages=pages;
+    this.readState=readState;
+}
+
+function addBookToLibrary(){
+    //adds books
+
+}
+
+//////////// Form input stuff ////////
+const submit = document.querySelector("#form-submit");
+const formElements = document.querySelectorAll("input");
+
+function resetForm() {
+    formElements.forEach(input=>{input.value=''});
+}
+
+submit.addEventListener('click',resetForm);
